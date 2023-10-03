920dcef43d0c21210bd9aea62ebf16559897d159
Shaundai Person <sperson@netflix.com>
2023-10-02 18:05:59 -0700

    remove avatar selection for new player (because of time)

--- src/components/Scoreboard/AddPlayer.tsx
+++ src/components/Scoreboard/AddPlayer.tsx
@@ -2,12 +2,11 @@ import { FormEvent, useState } from 'react'
 import './AddPlayer.css'
 
 interface AddPlayerProps {
-	handleAddPlayer: (newName: string, avatar: string) => void
+	handleAddPlayer: (newName: string) => void
 }
 
 export const AddPlayer = ({ handleAddPlayer }: AddPlayerProps) => {
 	const [playerName, setPlayerName] = useState<string>('')
-	const [isChoosingAvatar, setIsChoosingAvatar] = useState(false)
 
 	const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
 		e.preventDefault()
@@ -19,76 +18,19 @@ export const AddPlayer = ({ handleAddPlayer }: AddPlayerProps) => {
 		if (!playerName) {
 			return
 		} else {
-			setIsChoosingAvatar(true)
+			handleAddPlayer(playerName)
+			setPlayerName('')
 		}
 	}
-
-	const handleSubmitAvatar = (e: MouseEvent<HTMLDivElement, MouseEvent>) => {
-		e.preventDefault()
-		handleAddPlayer(playerName, e.target.title)
-		setPlayerName('')
-		setIsChoosingAvatar(false)
-	}
-
-	const ChooseAvatarSelection = () => {
-		return (
-			<div className='choose-avatar-container'>
-				<div
-					className='choose-avatar-button-container'
-					onClick={handleSubmitAvatar}>
-					<img
-						className='choose-avatar-button'
-						src='/src/assets/raphael.png'
-						alt='Michaelangelo'
-						title='michaelangelo'></img>
-				</div>
-				<div
-					className='choose-avatar-button-container'
-					onClick={handleSubmitAvatar}>
-					<img
-						className='choose-avatar-button'
-						src='/src/assets/raphael.png'
-						alt='Raphael'
-						title='raphael'
-					/>
-				</div>
-				<div
-					className='choose-avatar-button-container'
-					onClick={handleSubmitAvatar}>
-					<img
-						className='choose-avatar-button'
-						src='/src/assets/leonardo.png'
-						alt='Leonardo'
-						title='Leonardo'
-					/>
-				</div>
-				<div
-					className='choose-avatar-button-container'
-					onClick={handleSubmitAvatar}>
-					<img
-						className='choose-avatar-button'
-						src='/src/assets/donatello.png'
-						alt='Donatello'
-						title='Donatello'
-					/>
-				</div>
-				{`Choose an avatar for ${playerName}`}
-			</div>
-		)
-	}
-
 	return (
 		<form className='add-player-form' onSubmit={handleSubmit}>
-			{/* {!isChoosingAvatar ? ( */}
-				<input
-					className='add-player-input'
-					type='text'
-					value={playerName}
-					onChange={handleOnChange}
-				/>
-			{/* // ) : (
-			// 	<ChooseAvatarSelection />
-			// )} */}
+			{' '}
+			<input
+				className='add-player-input'
+				type='text'
+				value={playerName}
+				onChange={handleOnChange}
+			/>
 			<button className='add-player-button' value='Add Player'>
 				Add Player
 			</button>

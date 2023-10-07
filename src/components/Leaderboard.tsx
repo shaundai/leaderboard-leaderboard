ec66970b4df331d3a0086891e15d51fdb9997e5d
Shaundai Person <sperson@netflix.com>
2023-10-07 13:29:34 -0400

    adjust width of success banner

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -8,20 +8,23 @@ export const Leaderboard = () => {
 	const [votedPlayers, setVotedPlayers] = useState<string[]>([''])
 
 	return (
-		<>
-			{isSuccessBannerVisible && <SuccessBanner votedPlayers={votedPlayers} />}
-			<h1 className='title'>
-				<span className='leaderboard'>Leaderboard</span>
-				<span>Scoreboard</span>
-			</h1>
-			<h3 className='tagline'>
-				Vote for your fave leaderboard or enter your own leaderboard to the
-				competition!
-			</h3>
-			<PlayerList
-				setVotedPlayers={setVotedPlayers}
-				setIsSuccessBannerVisible={setIsSuccessBannerVisible}
-			/>
-		</>
+			<div className='container'>
+				{isSuccessBannerVisible && (
+					<SuccessBanner votedPlayers={votedPlayers} />
+				)}
+				<h1 className='title'>
+					<span className='leaderboard'>Leaderboard</span>
+					<span>Scoreboard</span>
+				</h1>
+
+				<h3 className='tagline'>
+					Vote for your fave leaderboard or enter your own leaderboard to the
+					competition!
+				</h3>
+				<PlayerList
+					setVotedPlayers={setVotedPlayers}
+					setIsSuccessBannerVisible={setIsSuccessBannerVisible}
+				/>
+			</div>
 	)
 }

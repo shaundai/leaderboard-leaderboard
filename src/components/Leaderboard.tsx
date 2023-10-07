ab0d611f500f8a989894fe8564c5a31bb9644526
Shaundai Person <sperson@netflix.com>
2023-10-07 12:27:08 -0400

    ..

--- src/components/Leaderboard.tsx
+++ src/components/Leaderboard.tsx
@@ -1,7 +1,7 @@
-import { PlayerList } from './scoreboard/PlayerList'
-import './Leaderboard.css'
-import { SuccessBanner } from './scoreboard/SuccessBanner'
 import { useState } from 'react'
+import { PlayerList } from './scoreboard/PlayerList.tsx'
+import { SuccessBanner } from './scoreboard/SuccessBanner.tsx'
+import './Leaderboard.css'
 
 export const Leaderboard = () => {
 	const [isSuccessBannerVisible, setIsSuccessBannerVisible] = useState(false)
@@ -18,7 +18,10 @@ export const Leaderboard = () => {
 				Vote for your fave leaderboard or enter your own leaderboard to the
 				competition!
 			</h3>
-			<PlayerList setVotedPlayers={setVotedPlayers} setIsSuccessBannerVisible={setIsSuccessBannerVisible} />
+			<PlayerList
+				setVotedPlayers={setVotedPlayers}
+				setIsSuccessBannerVisible={setIsSuccessBannerVisible}
+			/>
 		</>
 	)
 }
